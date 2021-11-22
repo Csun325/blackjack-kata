997233658033a7e06e6ba8e76a9cbcd2e6f51937
Csun325 <csun325@aucklanduni.ac.nz>
2021-11-23 09:27:29 +1300

    renamed files

---
+++ blackjack-kata/blackjack_kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blackjack_kata
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
